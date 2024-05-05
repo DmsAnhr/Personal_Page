dab5506e967f8bbf2835c35e4920639452ac182f
Dimas Atmodjo <dimasputra162003@gmail.com>
2024-05-06 00:37:30 +0700

    snapscroll

--- js/script.js
+++ js/script.js
@@ -1,30 +1,63 @@
+var scrollPos = 0;
+var scrollCount = 0;
+var timeout = null;
+var pageOffsets = [];
 
 $( document ).ready(function() {
     $(window).scrollTop(0);
-    
-    // var position = $(window).scrollTop();
-    // $(window).scroll(function() {
-    //     var scroll = $(window).scrollTop();
-    //     if(scroll > position) {
-    //         console.log('scrollDown');
-    //     } else {
-    //         console.log('scrollUp');
-    //     }
-    //     position = scroll;
-    // });
 
-    $(window).on('mousewheel DOMMouseScroll', function(e) {
-        var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+    $('.page[id!=page45]').each(function(){
+        var offset = $(this).offset().top;
+        var sectionId = $(this).attr('id');
+        console.log("Section " + sectionId + " : " + offset);
+        pageOffsets.push(offset);
+    });
 
-        // Jika mouse wheel digerakkan ke atas
-        if (delta > 0) {
-            // Lakukan sesuatu saat mouse wheel digerakkan ke atas
-            console.log('Mouse wheel digerakkan ke atas');
-        }
-        // Jika mouse wheel digerakkan ke bawah
-        else {
-            // Lakukan sesuatu saat mouse wheel digerakkan ke bawah
-            console.log('Mouse wheel digerakkan ke bawah');
+    console.log(pageOffsets);
+
+
+    $('.fixed a').on('click', function() {
+        var index = $(this).index();
+        var targetOffset = pageOffsets[index];
+        scrollPos = index;
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 100);
+    });
+
+    $(window).on('wheel', function(event) {
+        if (event.originalEvent.deltaY > 0) { 
+            // scroll ke bawah
+            scrollCount++;
+            if (scrollCount === 1) {
+                console.log('bawah');
+                scrollPos ++;
+                var targetOffset = pageOffsets[scrollPos];
+                $('html, body').animate({
+                    scrollTop: targetOffset
+                }, 100);
+            }
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                scrollCount = 0;
+            }, 500);
+        } else { 
+            // scroll ke atas
+            scrollCount++;
+            if (scrollCount === 1) {
+                console.log('atas');
+                scrollPos--;
+                var targetOffset = pageOffsets[scrollPos];
+                $('html, body').animate({
+                    scrollTop: targetOffset
+                }, 100);
+            }
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                scrollCount = 0;
+            }, 500);
         }
     });
+
+
 });
